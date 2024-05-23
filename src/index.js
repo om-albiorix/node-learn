7b5a5db5af2560d455a32524ae3790e4f95d0e27
om-albiorix <oms.albiorix@gmail.com>
2024-05-23 17:05:58 +0530

    successfully connected mongodb

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constant";
-// import express from "express";
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
-// const app =express();
+dotenv.config({
+    path:"./env"
+})
 
-// (async () => {
-//     try{
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",()=>{
-//             console.log("ERR",error)
-//             throw error
-//         })
-//         app.listen(process.env.PORT,()=>{
-//             console.log(`App is listening on port ${process.env.PORT}`)
-//         })
-//     }catch(error){
-//         console.log(error,"error")
-//         throw error
-//     }
-// })()
+console.log(process.env,"dotenv") 
+
+connectDB()
 
